1a07d1a5bc93ec24844cf8889d6e9fa9574dbb6f
4lexDeu5 <86321250+4lexDeu5@users.noreply.github.com>
2023-12-04 16:48:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ toast = Notification(app_id=ejecucion,
                      title="TEIA",
                      msg=mensaje,
                      duration="short",
-                     icon=r"" #Ruta del icono que se desea utilizar.
+                     icon=r"" #Ruta absoluta del icono que se desea utilizar.
                      )
 
 toast.add_actions(label="Abrir", launch=archivo)
